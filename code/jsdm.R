dd4867ebac6d50809bf0e5a76c2b627c0eb590d9
juansvs <juans.vs@gmail.com>
2024-08-19 13:49:37 +0200

    included line to select stations w>3 mos sampling. changed coding for lc in model to binary . added beetle section, currently just a copy of the sjSDM model. copied plotting code from analyses.qmd started spAbundance section

--- code/jsdm.R
+++ code/jsdm.R
@@ -5,10 +5,18 @@ library(tidyverse)
 camvect <- vect("Data/ct_covars.geojson")
 campts_db <- as.data.frame(camvect) %>% mutate(site = gsub("#", "", deployment_id), days = as.numeric(days))
 
-DAT <- read_csv("Data/records_wild.csv")
+data_raw <- read_csv("Data/ct_records.csv",
+                     col_types = cols_only(deployment_id = 'c', common_name = 'c', timestamp = col_datetime("%Y-%m-%d %H:%M") ))
+# include only cameras with more than we weeks (~3 months)
+cams_include <- summarise(data_raw, .by = deployment_id, t=max(timestamp)-min(timestamp)) %>% 
+  filter(t>28*3) %>% 
+  mutate(site = sub("#","",deployment_id)) %>% 
+  select(site) 
+DAT <- read_csv("Data/records_wild.csv") %>% inner_join(cams_include)
 DAT_wcovs <- left_join(DAT, campts_db)
 # create sp matrix
 occ_data_bin <- DAT %>% count(site, common_name) %>% 
+  filter(!grepl("uid",common_name)) %>% # remove 'uid' species (felids, opossums, peccaries, raccoons)
   mutate(n = (n>0)) %>% 
   pivot_wider(names_from = common_name, values_from = n, values_fill = 0) %>% 
   column_to_rownames("site") %>% 
@@ -17,6 +25,7 @@ occ_data_bin <- DAT %>% count(site, common_name) %>%
 env_mat <- DAT %>% distinct(site) %>% left_join(campts_db) %>% 
   select(site, easting, northing, ghm1, lc1,alt1, evi_mean1, lfdistance, distance) %>% 
   rename(x = easting, y = northing, landcov = lc1, alt = alt1, ghm = ghm1, pa_dist = distance, lgfor_dist = lfdistance) %>% 
+  mutate(landcov = ifelse(landcov==12,1,0)) %>% 
   column_to_rownames("site") %>% 
   as.matrix()
 
@@ -35,27 +44,67 @@ occ_models[["spatlin"]] <- sjSDM(Y = occ_data_bin, env = linear(data = scale(env
                                 se = TRUE, family=binomial("probit"), device = "cpu")
 
 anovas <- lapply(occ_models, anova)
+anovas
+# The ANOVA table for the models show that, for the base model, the abiotic
+# component has higher deviance than the biotic component, explaining 19% of the
+# explained variance, while biotic associations explain 12%. The full variance
+# explained is 36%.
+lapply(occ_models, logLik)
+# The highest log-likelihood is obtained with the base model, which has no
+# spatial component.
+
+ggobj <- plot(occ_models$base)
+ggobj+theme_classic(base_size = 16)
+# This model clearly shows the influence of altitude, species like tinamous,
+# tamanduas, armadillos, curassows, raccoons, and agoutis were associated with
+# lowlands, while oncillas, rabbits, jaguars, guans, and brocket were associated with
+# highlands.
+
+
+#### Beetle models ####
+DATb <- read.csv("Data/dung_beetles_prc.csv")
+DATb_wcovs <- inner_join(DATb, campts_db, by = join_by("Plot"=="site"))
+# create sp matrix
+occ_data_bin <- DAT %>% count(site, common_name) %>% 
+  mutate(n = (n>0)) %>% 
+  pivot_wider(names_from = common_name, values_from = n, values_fill = 0) %>% 
+  column_to_rownames("site") %>% 
+  as.matrix()
+# create env matrix
+env_mat <- DAT %>% distinct(site) %>% left_join(campts_db) %>% 
+  select(site, easting, northing, ghm1, lc1,alt1, evi_mean1, lfdistance, distance) %>% 
+  rename(x = easting, y = northing, landcov = lc1, alt = alt1, ghm = ghm1, pa_dist = distance, lgfor_dist = lfdistance) %>% 
+  mutate(landcov = ifelse(landcov==12,1,0)) %>% 
+  column_to_rownames("site") %>% 
+  as.matrix()
+
+occ_models <- list()
+
+# fit a basic model with no biotic interactions or spatial autocorrelation.
+occ_models[["base"]] <- sjSDM(Y = occ_data_bin, env = linear(data = scale(env_mat), formula = ~landcov+alt+ghm), # scales env. covariates: land cover (forest/grass), altitude, ghm, dist to pa
+                              se = TRUE, family=binomial("probit"), device = "cpu")
+SPeigen <- generateSpatialEV(env_mat[,c(1,2)])
+occ_models[["spatEV"]] <- sjSDM(Y = occ_data_bin, env = linear(data = scale(env_mat), formula = ~landcov+alt+ghm), # scales env. covariates: land cover (forest/grass), altitude, ghm, dist to pa
+                                spatial = linear(SPeigen, ~0+.),
+                                se = TRUE, family=binomial("probit"), device = "cpu")
+occ_models[["spatlin"]] <- sjSDM(Y = occ_data_bin, env = linear(data = scale(env_mat), formula = ~landcov+alt+ghm), # scales env. covariates: land cover (forest/grass), altitude, ghm, dist to pa
+                                 spatial = linear(env_mat, ~0+poly(x,y,degree=2)),
+                                 se = TRUE, family=binomial("probit"), device = "cpu")
+
+anovas <- lapply(occ_models, anova)
+anovas
 # The ANOVA table for the models show that, for the base model, the abiotic
 # component has higher deviance than the biotic component, explaining 18% of the
 # explained variance, while biotic associations explain 12%. The full variance
 # explained is the sum of the two, 33%.
-plot(anovas[[1]])
-# The highest log-likelihood is obtained with the base model, which has no
-# spatial component.
-plot(occ_models$base)
-# This model clearly shows the influence of altitude, species like tinamous,
-# tamanduas, armadillos, curassows, raccoons, and agoutis were associated with
-# lowlands, while oncillas, jaguars, guans, and brocket were associated with
-# highlands.
 
 
-## Species correlations
-spcor <- getCor(occ_models[[1]])
-image(spcor, asp=1, col = hcl.colors(12, 'Blue-Red2', rev=T), zlim = c(-1,1), 
-      bty='n',xaxt='n',yaxt='n') # remove box and axes
-mtext(occ_models[[1]]$species, 1,at = (0:30)/30, adj = 0, srt = )
+#### Plots ####
+# plot
+col2 <- colorRampPalette(c("#67001F", "#B2182B", "#D6604D", "#F4A582",
+                           "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE",
+                           "#4393C3", "#2166AC", "#053061"))
 
-## Plot
 bestmod <- occ_models$base
 bestmod_summary <- summary(bestmod)
 
@@ -67,11 +116,6 @@ sigLabel <- as.data.frame(t((bestmod_summary$P<0.05)*bestmod_summary$coefs)[,-1]
                                     x<0~"-"))) %>% 
   pivot_longer(cols = 2:4, names_to = "coef", values_to = "text")
 
-
-# plot
-col2 <- colorRampPalette(c("#67001F", "#B2182B", "#D6604D", "#F4A582",
-                           "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE",
-                           "#4393C3", "#2166AC", "#053061"))
 
 # arrange species by their dissimilarity for correlation matrix
 sp_hclust_order <- hclust(dist(cov2cor(getCov(bestmod))))$order
@@ -89,28 +133,28 @@ ggplot(bestmodcoefs, aes(x=coef, y=sp))+
 jsdm_coef_plot
 
 
-# import common names database
-common <- read.csv("Data/species_commonName.csv")
-common_hclust <- data.frame(common, order = sp_hclust_order)
+# # import common names database
+# common <- read.csv("Data/species_commonName.csv")
+# common_hclust <- data.frame(common, order = sp_hclust_order)
 
-corrmat <- matrix(nrow = length(sp_hclust_order), ncol = length(sp_hclust_order))
-for (i in seq_along(sp_hclust_order)) {
-  for (j in seq_along(sp_hclust_order)) {
-    corrmat[i,j] <- cov2cor(getCov(bestmod))[sp_hclust_order[i],sp_hclust_order[j]]
-  }
-}
+# corrmat <- matrix(nrow = length(sp_hclust_order), ncol = length(sp_hclust_order))
+# for (i in seq_along(sp_hclust_order)) {
+#   for (j in seq_along(sp_hclust_order)) {
+#     corrmat[i,j] <- cov2cor(getCov(bestmod))[sp_hclust_order[i],sp_hclust_order[j]]
+#   }
+# }
 
 # colnames(corrmat) <- rownames(corrmat) <- 
   
-  # base R plot
-  par(mar = c(2,15,10,5), las=2)
-plot(seq_along(sp_hclust_order), seq_along(sp_hclust_order), 
-     type = "n", 
-     xaxt = "n", yaxt = "n",bty="n",
-     xlab = "", ylab = "",asp=1)
-image(1:31,1:31,corrmat[ncol(corrmat):1,], add = T, col = hcl.colors(100,"Blue-Red 3", rev=T), zlim=c(-1,1))
-axis(2, at = 1:31, labels = bestmod$species[sp_hclust_order])
-axis(3, at = 31:1, labels = bestmod$species[sp_hclust_order])
+#   # base R plot
+#   par(mar = c(2,15,10,5), las=2)
+# plot(seq_along(sp_hclust_order), seq_along(sp_hclust_order), 
+#      type = "n", 
+#      xaxt = "n", yaxt = "n",bty="n",
+#      xlab = "", ylab = "",asp=1)
+# image(1:31,1:31,corrmat[ncol(corrmat):1,], add = T, col = hcl.colors(100,"Blue-Red 3", rev=T), zlim=c(-1,1))
+# axis(2, at = 1:31, labels = bestmod$species[sp_hclust_order])
+# axis(3, at = 31:1, labels = bestmod$species[sp_hclust_order])
 
 ## ggplot
 corr_ggdf <- expand.grid(bestmod$species[sp_hclust_order], bestmod$species[sp_hclust_order])
@@ -133,3 +177,10 @@ jsdm_corr_plot+theme(aspect.ratio = 1)
 
 cowplot::plot_grid(jsdm_coef_plot, jsdm_corr_plot, align = 'h', rel_widths = c(1,3))
 # need to check species name order
+
+#### spAbundance approach ####
+library(spAbundance)
+# We fit a model similar to the imperfect detection one implemented in TObler et
+# el. 2019, as implemented by the sfMsDS function in the spAbundance package
+abundform <- ~landcov+alt+ghm+(1|species)# formula for the abundance portion of the model
+detform <- ~(1|site)
